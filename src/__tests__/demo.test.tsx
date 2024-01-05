a49ac9724936b3b1dcbd4565d4b2cae7692bd50e
Daniel Anca <steptu94@gmail.com>
2024-01-05 11:50:55 +0200

    Adding the design for the footer.

--- src/__tests__/demo.test.tsx
+++ src/__tests__/demo.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, renderHook, act } from "@testing-library/react";
-import { Footer } from "../client/components/Footer";
+import { Footer } from "../client/components/Footer/Footer";
 import useCounter from "./testingHook";
 import { describe, test } from "vitest";
 describe("demo", () => {
