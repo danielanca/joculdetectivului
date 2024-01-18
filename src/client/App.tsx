04a4c0fb01911859e838decd0320dec7ed5d45a1
MuBBaSher-YaSiN <mubbasheryasin125@gmail.com>
2024-01-19 00:06:45 +0500

    Home Page Completed (Design + Search & Pagination)

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import Main from "./pages/Main";
-import { ContextWrapper } from "./Context";
-import { Footer } from "./components/Footer";
-import TopBar from "./components/TopBar";
-import Header from "./components/Header";
-import UserInfo from "./components/UserInfo";
-import UsersProfiles from "./components/UsersProfiles";
+import React from 'react';
+import Main from './pages/Main';
+import { ContextWrapper } from './Context';
+import { Footer } from './components/Footer';
+import TopBar from './components/TopBar';
+import Header from './components/Header';
+// import UserInfo from "./components/UserInfo";
+import UsersProfiles from './components/UsersProfiles';
 
 export const App = () => {
   return (
     <ContextWrapper>
       <TopBar />
       <Header />
-      <UserInfo />
+      {/* <UserInfo /> */}
       <UsersProfiles />
-      
+
       {/* <Main /> */}
       <Footer />
     </ContextWrapper>
